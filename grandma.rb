44650e1e29aa4e86f855e5b3be303473d29dd97f
Justine Bricknell <justinegayton@gmail.com>
2018-06-20 17:41:19 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,8 +4,8 @@ def speak_to_grandma(input)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif
     input == upcase
-    puts "NO, NOT SINCE 1938!"
+    return "NO, NOT SINCE 1938!"
   else
-    puts "HUH?! SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
